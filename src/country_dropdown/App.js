49e0d6931fc9cfaf3da98ec87e2631ea67745064
ann jaison <60734085+annmariajaison@users.noreply.github.com>
2020-11-02 09:51:48 +0530

    Create App.js

---
+++ src/country_dropdown/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CountrySelectDropdown from '../src/CountrySelectDropdown'
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+const App = () => {
+   return (
+     <View >
+      <CountrySelectDropdown/>
+      
+      </View>
+   )
+   
+}
+
+export default App
